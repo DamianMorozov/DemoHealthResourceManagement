5d214f2fda3a37a139f2ddfd47e957077ceee335
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-07-12 12:37:09 +0300

    Added HmSqlScripts/SCHEMA_REF/EMPLOYEES

---
+++ HmSqlScripts/SCHEMA_REF/EMPLOYEES/EMPLOYEES_CLEAR.sql
@@ -0,0 +1,44 @@
+------------------------------------------------------------------------------------------------------------------------
+-- EMPLOYEES_CLEAR
+------------------------------------------------------------------------------------------------------------------------
+SET NOCOUNT ON;
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+PRINT N'[ ] SET TRANSACTION ISOLATION LEVEL READ COMMITTED';
+DECLARE @DB_NAME_CUR VARCHAR(128) = NULL;
+DECLARE @DB_NAME_DEV VARCHAR(128) = 'HRM_DEMO_DEV';
+DECLARE @DB_NAME_PROD VARCHAR(128) = 'HRM_DEMO_PROD';
+DECLARE @SCHEMA_NAME VARCHAR(128) = 'REF';
+DECLARE @SCHEMA_ID INT = (SELECT [SCHEMA_ID] FROM [SYS].[SCHEMAS] WHERE [NAME] = @SCHEMA_NAME);
+DECLARE @CMD NVARCHAR(MAX);
+------------------------------------------------------------------------------------------------------------------------
+DECLARE @IS_ACTION BIT = 1;
+DECLARE @IS_COMMIT BIT = 0;
+------------------------------------------------------------------------------------------------------------------------
+IF (@IS_ACTION = 0) BEGIN
+	PRINT N'[x] ACTION IS DISABLED';
+END ELSE BEGIN
+	BEGIN TRAN
+	-- CHECK DB
+	SET @DB_NAME_CUR = (SELECT DB_NAME());
+	IF NOT (@DB_NAME_CUR = @DB_NAME_DEV OR @DB_NAME_CUR = @DB_NAME_PROD) BEGIN
+		PRINT N'[x] CURRENT DB [' + @DB_NAME_CUR + '] IS NOT CORRECT';
+	END ELSE BEGIN
+		PRINT N'[✓] CURRENT DB [' + @DB_NAME_CUR + '] IS CORRECT';
+		-- CLEAR
+		IF EXISTS (SELECT 1 FROM [SYS].[TABLES] WHERE [SCHEMA_ID] = @SCHEMA_ID AND [name] = N'EMPLOYEES') BEGIN
+			DELETE FROM [REF].[EMPLOYEES];
+			PRINT N'[✓] TABLE [' + @SCHEMA_NAME + '].[EMPLOYEES] WAS CLEARED: ' + CAST(@@rowcount AS VARCHAR) + ' ROWS WAS AFFECTED';
+		END;
+	END;
+	-- COMMIT
+	IF (@IS_COMMIT = 1) BEGIN
+		COMMIT TRAN;
+		PRINT N'[✓] JOB WAS COMMITTED';
+	END ELSE BEGIN
+		ROLLBACK TRAN;
+		PRINT N'[x] JOB WAS ROLLBACKED';
+	END;
+END;
+------------------------------------------------------------------------------------------------------------------------
---
+++ HmSqlScripts/SCHEMA_REF/EMPLOYEES/EMPLOYEES_SELECT.sql
@@ -0,0 +1,46 @@
+﻿------------------------------------------------------------------------------------------------------------------------
+-- EMPLOYEES_SELECT
+------------------------------------------------------------------------------------------------------------------------
+SET NOCOUNT ON;
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+PRINT N'[ ] SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED';
+DECLARE @DB_NAME_CUR VARCHAR(128) = NULL;
+DECLARE @DB_NAME_DEV VARCHAR(128) = 'HRM_DEMO_DEV';
+DECLARE @DB_NAME_PROD VARCHAR(128) = 'HRM_DEMO_PROD';
+DECLARE @SCHEMA_NAME VARCHAR(128) = 'REF';
+DECLARE @SCHEMA_ID INT = (SELECT [SCHEMA_ID] FROM [SYS].[SCHEMAS] WHERE [NAME] = @SCHEMA_NAME);
+DECLARE @CMD NVARCHAR(MAX);
+------------------------------------------------------------------------------------------------------------------------
+DECLARE @IS_ACTION BIT = 1;
+------------------------------------------------------------------------------------------------------------------------
+IF (@IS_ACTION = 0) BEGIN
+	PRINT N'[x] ACTION IS DISABLED';
+END ELSE BEGIN
+	-- CHECK DB
+	SET @DB_NAME_CUR = (SELECT DB_NAME());
+	IF NOT (@DB_NAME_CUR = @DB_NAME_DEV OR @DB_NAME_CUR = @DB_NAME_PROD) BEGIN
+		PRINT N'[x] CURRENT DB [' + @DB_NAME_CUR + '] IS NOT CORRECT';
+	END ELSE BEGIN
+		PRINT N'[✓] CURRENT DB [' + @DB_NAME_CUR + '] IS CORRECT';
+		-- SELECT
+		IF EXISTS (SELECT 1 FROM [SYS].[TABLES] WHERE [SCHEMA_ID] = @SCHEMA_ID AND [name] = N'EMPLOYEES') BEGIN
+			SELECT 
+				 [E].[ID] [EMPLOYEE_ID]
+				,[E].[DT_CREATE] [EMPLOYEE_DT_CREATE]
+				,[E].[DT_CHANGE] [EMPLOYEE_DT_CHANGE]
+				,[P].[FAMILY]
+				,[P].[NAME]
+				,[P].[SURNAME]
+				,[P].[GENDER]
+				,[P].[DT_BIRTH]
+				,[P].[CELLPHONE]
+				,[P].[EMAIL]
+			FROM [REF].[EMPLOYEES] [E]
+			INNER JOIN [REF].[PERSONS] [P] ON [E].[PERSON_ID] = [P].[ID]
+			ORDER BY [P].[FAMILY], [P].[NAME], [P].[SURNAME];
+		END;
+	END;
+END;
+------------------------------------------------------------------------------------------------------------------------
